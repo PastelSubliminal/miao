36eb2dc62896e8ed42ca7b6e3fb3180200fa5c35
Shi Yuhao <254690447@qq.com>
2020-04-30 02:55:02 +0800

    lodash

--- lodash/pastelsubliminal-lodash.js
+++ lodash/pastelsubliminal-lodash.js
@@ -1,4 +1,23 @@
 var pastelsubliminal = {
+    chunk:function(array, size=1){
+        var index = 0;
+        var resIndex = 0;
+        var result = new Array(Math.ceil(array.length / size));
+        for(var i = 0; i < array.length; i += size){
+            result[resIndex++] = array.slice(i, i + size);
+        }
+        return result;
+    },
+    compact:function(array){
+        var result = [];
+        for(var i = 0; i < array.length; i++){
+            if(array[i]) result.push(array[i]);
+        }
+        return result;
+    },
+    concat:function(array, ...values){
+
+    },
     isNaN:function(value){
         return this.isNumber(value) && value != value;
     },
