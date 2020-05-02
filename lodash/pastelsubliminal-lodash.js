1715f2a4125d569f30820b2d953c27d76415b827
Shi Yuhao <254690447@qq.com>
2020-05-02 16:25:20 +0800

    lodash

--- lodash/pastelsubliminal-lodash.js
+++ lodash/pastelsubliminal-lodash.js
@@ -1,23 +1,58 @@
 var pastelsubliminal = {
     chunk:function(array, size=1){
-        var index = 0;
-        var resIndex = 0;
-        var result = new Array(Math.ceil(array.length / size));
-        for(var i = 0; i < array.length; i += size){
+        let index = 0;
+        let resIndex = 0;
+        let result = new Array(Math.ceil(array.length / size));
+        for(let i = 0; i < array.length; i += size){
             result[resIndex++] = array.slice(i, i + size);
         }
         return result;
     },
     compact:function(array){
-        var result = [];
-        for(var i = 0; i < array.length; i++){
+        let result = [];
+        for(let i = 0; i < array.length; i++){
             if(array[i]) result.push(array[i]);
         }
         return result;
     },
     concat:function(array, ...values){
+        return array = array.concat(...values);
+    },
+    difference:function(array, ...value){
+        let result = array.slice();
+        value = [].concat(...value);
+        result = result.filter(n =>
+            !value.includes(n)
+            )
+        return result;
+    },
+    drop:function(array, n=1){
+        if(n >= array.length) return [];
+        return array.slice(n);
+    },
+    dropRight:function(array, n=1){
+        // if(n >= array.length) return [];
+        // return array.reverse().slice(n).reverse();
+        return array.length > n ? array.slice(0, array.length - n) : [];
+    },
+    dropRightWhile:function(array, predicate){
 
     },
+    dropWhile:function(array, predicate){
+
+    },
+    fill:function(array, value, start=0, end=array.length){
+        for(var i = start; i < end; i++){
+            array[i] = value;
+        }
+        return array;
+    },
+    findIndex:function(array, predicate, fromIndex){
+        for(var i = 0; i < array.length; i++){
+
+        }
+    },
+
     isNaN:function(value){
         return this.isNumber(value) && value != value;
     },
